8aba6cb697a4575f828f14f37bbec97ee5346d35
ChristianPr1nce <143338539+ChristianPr1nce@users.noreply.github.com>
2024-05-21 04:31:05 +0000

    test

--- test.py
+++ test.py
@@ -1,7 +1,24 @@
-age = int(input("Enter age: "))
-hah = 19
+import datetime
+# age = int(input("Enter age: "))
+# hah = 19
 
-if age > hah:
-    print("welcome")
+# if age > hah:
+#     print("welcome")
+# else:
+#     print("You're not allowed")
+
+current_time = datetime.datetime.now()
+
+current_day = current_time.year
+
+a = int(input("what is your age?"))
+b = int(input("what age would you like to retire?"))
+
+c = a - b 
+d = current_day + b
+if a == b:
+    print("YOU CAN RETIRE NOW!!!")
+elif a > b:
+    print("PLEASE DO THIS PROPERLY!!!")
 else:
-    print("You're not allowed")
+    print (f"you have {c} years left until you retire.\n you can retire in {d}")

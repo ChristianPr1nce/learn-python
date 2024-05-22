2dce27b5e20dc1859b7b40abaceab3d48a24024c
ChristianPr1nce <143338539+ChristianPr1nce@users.noreply.github.com>
2024-05-22 10:12:50 +0000

    hahah

--- hello-world.py
+++ hello-world.py
@@ -1,8 +1,15 @@
-a = int(float(input("What is the length of the room in feet?")))
-b = int(float(input("What is the width of the room in feet?")))
-c = a * b 
-d = c * 0.092903
-if isinstance(a, float) and isinstance(b, float):
-    print("WHAT ARE YOU DOING?")
-else:
-    print(f'you entered dimensions of {a} by {b} feet.\n The area is \n {c} square feet \n {d} square meters')
+# a = int(float(input("What is the length of the room in feet?")))
+# b = int(float(input("What is the width of the room in feet?")))
+# c = a * b 
+# d = c * 0.092903
+# if isinstance(a, float) and isinstance(b, float):
+#     print("WHAT ARE YOU DOING?")
+# else:
+#     print(f'you entered dimensions of {a} by {b} feet.\n The area is \n {c} square feet \n {d} square meters')
+
+
+try:
+    a = int(float(input("What is the length of the room in feet?")))
+    print(a)
+except:
+    print("what the fuck nigga thats not a number.")

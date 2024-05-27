110e73fbe5780d03da978c06f6dc71ed1065bec0
ChristianPr1nce <143338539+ChristianPr1nce@users.noreply.github.com>
2024-05-27 08:12:07 +0000

    pizza

--- pizza.py
+++ pizza.py
@@ -2,7 +2,7 @@ try:
     boxes = int(input("how many pizza boxes do you have?"))
     people = int(input("how many people?"))
     slices = boxes * 12
-    eachslice = int(slices / people)
+    eachslice = int(slices // people)
     leftovers = slices % people
     print(f"{boxes} boxes and {people} people \n {eachslice} slices each \n {leftovers} leftovers")
 except:

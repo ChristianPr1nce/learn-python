d16e652bce64631e77c7392d4cfd132bbe1d9f3f
ChristianPr1nce <143338539+ChristianPr1nce@users.noreply.github.com>
2024-06-04 20:28:11 +0000

    in lesgoo

---
+++ in.py
@@ -0,0 +1,7 @@
+userinput = (input("is your name in the list?"))
+namelist = ("nigga", "asshole", "john", "christian", "jet", "teddy")
+if userinput in namelist:
+    print(f"hello! {userinput}")
+else:
+    print("get the fuck out of here lil bro")
+

9bc2eb0b666f9063a261ccc4f6058c7d38488d3a
ChristianPr1nce <143338539+ChristianPr1nce@users.noreply.github.com>
2024-06-22 10:07:14 +0000

    eee

--- range.py
+++ range.py
@@ -1,2 +1,9 @@
- for NumberCounter = range in (1, 100 ,69):
+
+names = input("\n africans \n westernpeople \n eastern people \n Select A Category")
+
+africans = ["nigga, criminal, tall, slave, biggestdicksize"]
+westernpeople = ["rich, colonizer, white, averagedicksize"]
+easternpeople = ["masterofmath, smalleyes, decentwealth, firstcivilization, smallestdick"]
+
+for NumberCounter in africans:
     print(NumberCounter)

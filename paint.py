c5b518b099befbed7a58c7eb647e7eb90e819a1f
ChristianPr1nce <143338539+ChristianPr1nce@users.noreply.github.com>
2024-07-03 05:11:07 +0000

    i hate rounds

--- paint.py
+++ paint.py
@@ -2,12 +2,10 @@ try:
    length = int(input("What is the length of the Ceiling?"))
    width = int(input("What is the width of the Ceiling?"))
    squarefeet = length * width
-   paint = 32.516064
-   gallon = 1 
-   # gallon cover area = 350sqf
-   # if paint => squarefeet:
-   #  paint 
-   # 
+   paint = 350
+   gallon = squarefeet / paint
+   >>> round(gallon)
+   
    print(f"you will need {gallon} gallons of \n paint to cover {squarefeet} squarefeet")
 except:
    print("PLEASE ONLY ADD NUMBERS") 

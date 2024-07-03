1a524d35dca8be6c095e556e406ad895a33d1d1e
ChristianPr1nce <143338539+ChristianPr1nce@users.noreply.github.com>
2024-07-03 05:28:17 +0000

    yayyy

--- paint.py
+++ paint.py
@@ -1,10 +1,15 @@
 try:
+   #ask user lenght
    length = int(input("What is the length of the Ceiling?"))
+   #ask user width
    width = int(input("What is the width of the Ceiling?"))
+   # length * width to squarefeet
    squarefeet = length * width
+   # set paint variable
    paint = 350
+   # set gallon variable
    gallon = squarefeet / paint
-
+   #print results
    print(f"you will need {round(gallon)} gallons of \n paint to cover {squarefeet} squarefeet")
 except:
    print("PLEASE ONLY ADD NUMBERS") 

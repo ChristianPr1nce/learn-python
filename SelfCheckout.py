16c3581f1397fc71a4139c36c0a913fccf1ad606
ChristianPr1nce <143338539+ChristianPr1nce@users.noreply.github.com>
2024-07-04 21:53:40 +0000

    uhh

--- SelfCheckout.py
+++ SelfCheckout.py
@@ -1,6 +1,13 @@
-price = int(input("enter the price of an item:"))
-quantity = int(input("enter the quantity of an item 1:"))
-price2 = int(input("enter the price of item 2:"))
-quantity2 = int(input("enter the quantity of item 2:"))
-price3 = int(input("enter the price of item 3:"))
-quantity3 = int(input("enter the quantity of item 3:"))
+try:
+    price = int(input("enter the price of an item:"))
+    quantity = int(input("enter the quantity of an item 1:"))
+    price2 = int(input("enter the price of item 2:"))
+    quantity2 = int(input("enter the quantity of item 2:"))
+    price3 = int(input("enter the price of item 3:"))
+    quantity3 = int(input("enter the quantity of item 3:"))
+
+    #tax = 5.5%
+    #pricequantity = price * quantity 
+    #price2quantity = price2 * quantity2
+    #price3quantity = price3 * quantity3
+    #
